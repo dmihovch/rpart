82e22d2fa173ca4e389556aac2cbd8620001d422
Daniel Mihovch <dmihovch@gmail.com>
2026-01-19 22:20:31 -0500

    phsyics

--- include/physics.h
+++ include/physics.h
@@ -3,10 +3,11 @@
 #include "particle.h"
 #include "constants.h"
 #include "maths.h"
+#include <math.h>
 void update_particles(Particle* p, int particle_count);
 void reset_accelerations(Particle* p, int particle_count);
 void accumulate_forces(Particle* p, int particle_count);
 void move_particles_handle_walls(Particle* p, int particle_count);
 void handle_particle_collisions(Particle* p, int particle_count);
-bool check_collisions_circles(Vector2 apos, float ar, Vector2 bpos, float br);
+Vector2 check_collisions_circles(Vector2 apos, float ar, Vector2 bpos, float br);
 #endif //PHYSICS_H
