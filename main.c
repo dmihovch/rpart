387e4eadac866e8fad45fe5893256358f0766229
Daniel Mihovch <dmihovch@gmail.com>
2026-01-21 08:15:06 -0500

    inputs!

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "include/drawing.h"
 #include "include/particle.h"
 #include "include/physics.h"
+#include <raylib.h>
 
 
 int main(int argc, char** argv){
@@ -18,26 +19,40 @@ int main(int argc, char** argv){
 		CloseWindow();
 		return 1;
 	}
+	double frametime_start;
+	double render_start;
+	double update_start;
+	double frametime_end;
+	double render_end;
+	double update_end;
 	
-
+	bool running = true;
 	SetTargetFPS(FPS);
 	while(!WindowShouldClose() && GetKeyPressed() != KEY_Q)
 	{
-		double frametime_start = GetTime();
+		if(IsKeyPressed(KEY_P))
+		{
+			running = !running;
+			printf("toggled\n");
+		}
+
+		frametime_start = GetTime();
 		BeginDrawing();
 		ClearBackground(GRAY);
-		
-		double render_start = GetTime();
-		draw_particles(particles, particles_count);
-		double render_end = GetTime();
-			
-		double update_start = GetTime();
-		update_particles(particles, particles_count);
-		double update_end = GetTime();
+		if(running)
+		{
+			render_start = GetTime();
+			draw_particles(particles, particles_count);
+			render_end = GetTime();
 
+			update_start = GetTime();
+			update_particles(particles, particles_count);
+			update_end = GetTime();
+		}
+				
 		DrawFPS(0, 0);
 
-		double frametime_end = GetTime();
+		frametime_end = GetTime();
 		
 		draw_diagnostics(frametime_start, frametime_end, render_start, render_end, update_start, update_end);
 		EndDrawing();
